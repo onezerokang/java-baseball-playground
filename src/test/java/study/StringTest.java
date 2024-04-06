0c78af65d8cd49eefe271658054a81d9814b64ec
Wonyoung Kang <onezerokang@gmail.com>
2024-04-06 16:52:29 +0900

    0. 학습테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,67 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-public class StringTest {
+class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /* 요구사항 1
+     * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+     * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+     */
+    @Test
+    void split1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("2", "1");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    /* 요구사항 2
+     * "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+     */
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    /* 요구사항 3
+     * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+     * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+     * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+     */
+    @DisplayName("String의 charAt() 메소드로 특정 위치의 문자를 가져올 수 있다.")
+    @Test
+    void charAt1() {
+        String str = "abc";
+        char c1 = str.charAt(0);
+        char c2 = str.charAt(1);
+        char c3 = str.charAt(2);
+
+        assertThat(c1).isEqualTo('a');
+        assertThat(c2).isEqualTo('b');
+        assertThat(c3).isEqualTo('c');
+    }
+
+    @DisplayName("String의 charAt() 메소드로 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다. ")
+    @Test
+    void charAt2() {
+        String str = "abc";
+        assertThatThrownBy(() -> str.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
